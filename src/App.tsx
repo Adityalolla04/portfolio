e5896365f76a307dad017290d3387349f638c776
Lolla Aditya Srivatsav <adityasrivatsav04@gmail.com>
2025-03-04 15:45:10 -0500

    Critical Commits

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/NavBar";
 import Home from "./pages/Home";
 import About from "./pages/About";
@@ -7,13 +7,15 @@ import About from "./pages/About";
 
 const App: React.FC = () => {
   return (
-    <Router>
+    <>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        {/* ✅ Add a fallback route for undefined paths */}
+        <Route path="*" element={<Home />} />
       </Routes>
-    </Router>
+    </>
   );
 };
 
